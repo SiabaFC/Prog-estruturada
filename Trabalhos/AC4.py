d9a3e44ef1c54cf80730e5b0722c6ad95f5f4a24
Felipe Siaba <160870389+SiabaFC@users.noreply.github.com>
2024-03-12 16:54:24 -0300

    Update AC4.py
    Alteração final

--- Trabalhos/AC4.py
+++ Trabalhos/AC4.py
@@ -14,15 +14,6 @@ AC4
 """
 
 
-
-"""
-Programação Estruturada
-2024.1
-08/03/2024
-
-AC4
-"""
-
 def ler_nome():
     """Retorna o nome do aluno inserido pelo usuário."""
     return input("Informe o seu nome: ")
@@ -40,15 +31,15 @@ def analisar_subst(ap1, ap2, asub):
     Retorna as duas notas a serem usadas no cálculo.
     A AS deve substituir a AP1 ou AP2 se for maior que elas.
     """
-    if ap1 <= ap2 and asub>ap1:
+    if ap1 <= ap2 and asub >= ap1:
         return asub,ap2
-    if ap2 <=ap1 and asub > ap2:
+    if ap2 <=ap1 and asub >= ap2:
         return ap1, asub
 
 def calcular_media(ap1, ap2, asub, ac):
     """Calcula e retorna a média final do aluno."""
     prova1, prova2 = analisar_subst(ap1, ap2, asub)
-    media = (prova1 + prova2) * 0.4 + ac * 0.2
+    media = (prova1 + prova2) * 0.4 + (ac * 0.2)
     return media
 
 def aluno_foi_aprovado(media):
@@ -57,6 +48,15 @@ def aluno_foi_aprovado(media):
 
 def notas_sao_validas(ap1, ap2, asub, ac):
     """Retorna True se todas as notas estão entre 0 e 10, inclusive."""
+    if ap1 >= 0 and ap1 <= 10:
+      return ap1
+    if ap2 >= 0 and ap2 <= 10:
+      return ap2
+    if asub >= 0 and asub <= 10:
+      return asub
+    if ac >= 0 and ac <= 10:
+      return ac
+      
     
 
 def main():
@@ -72,10 +72,3 @@ def main():
                 print("Aluno foi reprovado.")
 
 main()
-
-
-
-
-
-
-
